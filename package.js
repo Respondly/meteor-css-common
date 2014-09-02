453adbc0a08071e327c89d98adf59b9b27bfee94
Phil Cockfield <phil@cockfield.net>
2014-09-02 14:37:30 +1200

    Loading Google Web fonts via code.

--- package.js
+++ package.js
@@ -5,9 +5,12 @@ Package.describe({
 
 
 Package.on_use(function (api) {
+  api.use(['coffeescript', 'sugar']);
   api.use(['css-stylus']);
+  api.export('Fonts');
 
   // Generated with: github.com/philcockfield/meteor-package-paths
+  api.add_files('shared/ns.js', ['client', 'server']);
   api.add_files('shared/_normalize/normalize.css', 'client');
   api.add_files('shared/_normalize/normalize.css', 'server', { isAsset:true });
   api.add_files('shared/classes/alerts.styl', 'client');
@@ -20,6 +23,8 @@ Package.on_use(function (api) {
   api.add_files('shared/css-mixins/alert.import.styl', 'server', { isAsset:true });
   api.add_files('shared/css-mixins/core.import.styl', 'client');
   api.add_files('shared/css-mixins/core.import.styl', 'server', { isAsset:true });
+  api.add_files('server/fonts.coffee', 'server');
+  api.add_files('client/fonts.coffee', 'client');
 
 });
 
